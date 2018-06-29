1cc5bf55336ea71108804b05819fe7f8f2eef77e
Nahuel Aquino <30450134+nahuel89aquino@users.noreply.github.com>
2018-06-29 13:51:43 -0200

    ejercicio 5 ficha 8

---
+++ EJERCICIOS_FICHA_8/Ejercicio_05.py
@@ -0,0 +1,17 @@
+#1) Cantidad de palabras que comienzan con consonantes y terminan en vocales
+#2) Cantidad  de palabras que poseen la secuencia 'li'a partir de la 
+#tercera letra de la palabra
+#3) Cantidad de palabras con menos de cuatro letras y porcentaje que dicha
+#cantidad representa sobre el total del texto
+
+texto = "hoy es un buen dia para comer una asado"
+texto += "."
+
+ft_
+def consonante(x):
+    pass
+
+print(texto)
+for i in texto:
+    if i != " " and i != ".":
+        if not ft_letra:
