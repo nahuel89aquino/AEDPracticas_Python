017124fa4bb8c1070ed3c62a265e0e8618950f6e
Nahuel Aquino <nahuel89.aquino@gmail.com>
2018-07-28 16:12:11 -0300

    finalizacion del ejercicio 7 ficha 9

--- EJERCICIOS_FICHA_9/Ejercicio_07.py
+++ EJERCICIOS_FICHA_9/Ejercicio_07.py
@@ -1,19 +1,22 @@
-#Desarrollar un programa controlado por menú de opciones, que permita:
-#a) Tablas: ingresar por teclado un valor positivo entre 1 y 10 (validarlo).
+# Desarrollar un programa controlado por menú de opciones, que permita:
+# a) Tablas: ingresar por teclado un valor positivo entre 1 y 10 (validarlo).
 # Mostrar la tabla de multiplicar correspondiente a dicho número.
 # (Por ej: si se ingresa 3, mostrar 3x1=3, 3x2=6 etc.)
-#b) Mayor y menor: ingresar por teclado una sucesión de números positivos (validar),
+# b) Mayor y menor: ingresar por teclado una sucesión de números positivos (validar),
 # que termina cuando se ingresa 0. Informar mayor y menor valor de la sucesión.
-#c) Múltiplos: ingresar por teclado dos valores a y b, siendo a positivo y b mayor que a (validarlo).
+# c) Múltiplos: ingresar por teclado dos valores a y b, siendo a positivo y b mayor que a (validarlo).
 # Sumar todos los múltiplos de a comprendidos en el intervalo [a,b]
-#d) Texto: ingresar por teclado un texto terminado en un punto (validar esto),
+# d) Texto: ingresar por teclado un texto terminado en un punto (validar esto),
 # cuyas palabras se separan por un único espacio.
 # Informar cuántas palabras terminan en vocal y qué porcentaje representan sobre el total
 # de palabras del texto.
+
+
 def tablas(n):
     for i in range(1, 10 + 1):
         tabla = str(i) + ' * ' + str(n)
         print(tabla + " = " + str(eval(tabla)))
+
 
 def mayor_menor(n):
     may = n[0]
@@ -24,6 +27,8 @@ def mayor_menor(n):
         if i < men:
             men = i
     return may, men
+
+
 def multiplos(a, b):
     mul = 0
     for i in range(a, b + 1):
@@ -31,8 +36,30 @@ def multiplos(a, b):
             mul += i
     return mul
 
+
+def vocal(k):
+    return k in ("a", "e", "i", "o", "u")
+
+
+def textos(tex):
+    cletra = cvol = 0
+    hay_vocal = False
+    for i in tex:
+        if not (i == " " or i == "."):
+            cletra += 1
+            hay_vocal = False
+            if vocal(i):
+                hay_vocal = True
+        else:
+            if hay_vocal:
+                cvol += 1
+            hay_vocal = False
+    porc = round(cvol * 100 / cletra, 2)
+    return cvol, porc
+
+
 def test():
-    opciones= "1234"
+    opciones = "1234"
     k = -1
     numeros = tuple()
     print("1) Tablas")
@@ -40,7 +67,7 @@ def test():
     print("3) Multiplos")
     print("4) Texto")
     n = input()
-    #validacion de opcion principal
+    # validacion de opcion principal
     while n not in opciones:
         print(Error1)
         n = input("Elije una opcion nuevamente: ")
@@ -82,8 +109,15 @@ def test():
         mul = multiplos(int(a), int(b))
         print("La suma de los multiplos de 'a' comprendidios en el intervalo son: ", mul)
 
+    if n == "4":
+        texto = input("Ingrese un texto:")
+        texto += "."
+        t_voc, porc_voc = textos(texto)
+        print("La cantidad de vocales del texto son", t_voc)
+        print("El porcentaje de vocales que representan sobre el total de palabras es: %", porc_voc)
 
 
+# texto = "hoy es un lindo dia para comer un asado"
 Error1 = "Error: opcion no valida"
 Error2 = "Error: valor invalido"
 test()
